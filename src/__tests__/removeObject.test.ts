02660f7eb7b78fdf0c50a6ac6d40405c4e06e241
Madhukant Dinker <madhukant.dinker@avisbudget.com>
2023-02-03 17:27:07 +0530

    Added removeEmptyObjects : Madhukant

--- src/__tests__/removeObject.test.ts
+++ src/__tests__/removeObject.test.ts
@@ -1,4 +1,26 @@
-import { Greeter } from '../index';
-test('My Greeter', () => {
-  expect(Greeter('Carl')).toBe('Hello Carl');
+import { removeEmptyObjects } from '../index';
+
+let testData = {
+	"a" : "madhukant",
+	"b" : "dinker",
+	"c" : {
+		"d" : [{}, {}, {}],
+		"e" : {
+			"f" : {},
+			"g" : {},
+			"h" : {}
+		}
+	}
+}
+
+let expectedData = {
+	"a" : "madhukant",
+	"b" : "dinker",
+	"c" : {
+		"d" : [,,,]
+	}
+}
+
+test('My Testing Madhukant', () => {
+  expect(removeEmptyObjects(testData)).toStrictEqual(expectedData);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+/*
+removeEmptyObjectsFromResponse will accept Object as argument
+and return cleaned Object. The cleaning processing will remove
+undefined, '', NaN, etc
+@Author - Madhukant Dinker
+@Date - 03/Feb/2023
+*/
+export function removeEmptyObjects(inData: any) {
+//Loop through the rool attributes and start checking for empty object
+Object.keys(inData).forEach((key) => {
+  // If it's Object
+  if (inData[key] && typeof inData[key] === 'object') {
+    // Call the same function with new Object
+    const childObject = removeEmptyObjects(inData[key]);
+    // Delete if there is no child inside Object
+    if (childObject === undefined) {
+      delete inData[key];
+    }
+  // Else it may be Blank, null, undefined so delete
+  } else if (inData[key] === '' || inData[key] === null || inData[key] === undefined) {
+    delete inData[key];
+  }
+});
+// Return in case of data present or it's array with data either undefined
+return Object.keys(inData).length > 0 || inData instanceof Array ? inData : undefined;
+};
