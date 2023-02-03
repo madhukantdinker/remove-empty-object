ed8041cb5135e38b2c8af3f34dec2c879d6ef674
Madhukant Dinker <madhukant.dinker@avisbudget.com>
2023-02-03 17:29:45 +0530

    2.0.1

--- src/__tests__/removeObject.test.ts
+++ src/__tests__/removeObject.test.ts
@@ -1,25 +1,25 @@
 import { removeEmptyObjects } from '../index';
 
 let testData = {
-	"a" : "madhukant",
-	"b" : "dinker",
-	"c" : {
-		"d" : [{}, {}, {}],
-		"e" : {
-			"f" : {},
-			"g" : {},
-			"h" : {}
-		}
-	}
-}
+  a: 'madhukant',
+  b: 'dinker',
+  c: {
+    d: [{}, {}, {}],
+    e: {
+      f: {},
+      g: {},
+      h: {},
+    },
+  },
+};
 
 let expectedData = {
-	"a" : "madhukant",
-	"b" : "dinker",
-	"c" : {
-		"d" : [,,,]
-	}
-}
+  a: 'madhukant',
+  b: 'dinker',
+  c: {
+    d: [, , ,],
+  },
+};
 
 test('My Testing Madhukant', () => {
   expect(removeEmptyObjects(testData)).toStrictEqual(expectedData);
--- src/index.ts
+++ src/index.ts
@@ -6,21 +6,21 @@ undefined, '', NaN, etc
 @Date - 03/Feb/2023
 */
 export function removeEmptyObjects(inData: any) {
-// Loop through the rool attributes and start checking for empty object
-Object.keys(inData).forEach((key) => {
-  // If it's Object
-  if (inData[key] && typeof inData[key] === 'object') {
-    // Call the same function with new Object
-    const childObject = removeEmptyObjects(inData[key]);
-    // Delete if there is no child inside Object
-    if (childObject === undefined) {
+  // Loop through the rool attributes and start checking for empty object
+  Object.keys(inData).forEach((key) => {
+    // If it's Object
+    if (inData[key] && typeof inData[key] === 'object') {
+      // Call the same function with new Object
+      const childObject = removeEmptyObjects(inData[key]);
+      // Delete if there is no child inside Object
+      if (childObject === undefined) {
+        delete inData[key];
+      }
+      // Else it may be Blank, null, undefined so delete
+    } else if (inData[key] === '' || inData[key] === null || inData[key] === undefined) {
       delete inData[key];
     }
-  // Else it may be Blank, null, undefined so delete
-  } else if (inData[key] === '' || inData[key] === null || inData[key] === undefined) {
-    delete inData[key];
-  }
-});
-// Return in case of data present or it's array with data either undefined
-return Object.keys(inData).length > 0 || inData instanceof Array ? inData : undefined;
-};
+  });
+  // Return in case of data present or it's array with data either undefined
+  return Object.keys(inData).length > 0 || inData instanceof Array ? inData : undefined;
+}
